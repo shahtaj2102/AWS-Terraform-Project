1de55b716eb99f32b40d0e59fc4e418670207527
Shahtaj singh Gill <157941958+shahtaj2102@users.noreply.github.com>
2026-07-28 11:10:05 -0400

    Add ALB DNS name output to Terraform configuration

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
+
+output "alb_dns_name" {
+  description = "Public DNS name of the Application Load Balancer"
+  value       = aws_lb.app_alb.dns_name
+}
